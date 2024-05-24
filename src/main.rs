38918452826b70c1b83c9df47ea63d786eb02837
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-05-24 23:12:41 +0300

    fix dotenv

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::{env, time::Duration};
-use dotenv::dotenv;
 use eyre::{Context, Ok, Result};
 mod notification;
 use reqwest::blocking::ClientBuilder;
@@ -10,7 +9,8 @@ mod github;
 fn main() -> Result<()> {
     env_logger::init();
     log::info!("App start");
-    dotenv()?;
+    let env_path = env::current_exe().unwrap().parent().unwrap().join(".env");
+    dotenv::from_path(env_path)?;
     let github_token = env::var("GITHUB_TOKEN").context("Please set GITHUB_TOKEN in .env")?;
     let ntfy_url = env::var("NTFY_URL").context("Please set NTFY_URL in .env")?;
     let mut state = State::try_create().unwrap();
